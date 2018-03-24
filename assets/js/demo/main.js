c0a039341c7c00bcec378a8f09b5cf18d66868ca
PhungHV <phunghv@dyno.vn>
2018-03-25 04:00:46 +0700

    add demo

--- assets/js/demo/main.js
+++ assets/js/demo/main.js
@@ -1,18 +1,4 @@
-console.log("start js demo")
-function getAudio() {
-    var xhttp = new XMLHttpRequest();
-    var voice = $('input[name=voice]:checked').val();
-    var speed = $('input[name=speed]').val();
-    var content = $('textarea[name=dm-content]')
-                    .val()
-                    .replace(/ /g, '+')
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("dm-result").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("POST", "https://ailab.hcmus.edu.vn/vos/demo.php", true);
-    var data = "text="+content+"&speed="+speed+"&voice"+voice;
-    console.log(data);
-    xhttp.send(data);
+function getAnwser(){
+  var MAX = 100;
+  var x = Math.floor((Math.random() * MAX));
 }
